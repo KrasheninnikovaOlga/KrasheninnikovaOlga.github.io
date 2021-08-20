a53cfed0f78e49c7ee7c918d64428cbf17dbf36d
Olga Krasheninnikova <krasheninnikova.olga@gmail.com>
2021-08-20 11:03:53 -0700

    deploy v1

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "styles/_reset.scss";
-import Resume from "components/Resume/Resume.jsx";
-
-const root = document.getElementById("Root");
-
-ReactDOM.render(<Resume />, root);
